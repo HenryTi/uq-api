214bda4eacbd9e2583ae9a63945f655994cee35a
HenryTi <liaohengyi@gmail.com>
2022-01-27 18:48:03 -0500

    ActIX

--- out/core/builder/my/mySqlBuilder.js
+++ out/core/builder/my/mySqlBuilder.js
@@ -182,7 +182,7 @@ class MySqlBuilder {
                 }
             }
             else {
-                sql += `set @id=\`tv_${name}$id\`(@unit,@user,1, null`;
+                sql += `set @id=\`tv_${name}$id\`(@unit,@user,1`; // 2022-1-27 , null 之前好像要加上这个。现在不要加。？？
                 let updateOverride = { id: '@id' };
                 if (keys.length > 0) {
                     function sqlFromKey(keyName, type, v) {
