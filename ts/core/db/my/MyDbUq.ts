062cea6c99ae16b0895f2771864806e9325b7dfe
HenryTi <liaohengyi@gmail.com>
2023-01-29 20:07:42 -0500

    twProfix

--- ts/core/db/my/MyDbUq.ts
+++ ts/core/db/my/MyDbUq.ts
@@ -40,6 +40,7 @@ export class MyDbUq extends MyDb implements DbUq {
         const { oldTwProfix, tv$entityExists } = sqlsVersion;
         let ret = await this.sql(tv$entityExists, [this.name]);
         this.twProfix = ret.length > 0 ? oldTwProfix : '';
+        console.error(this.twProfix);
     }
 
     private resetProcColl() {
