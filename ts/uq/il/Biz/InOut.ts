31effaa70941be775686a14217b183b3a48a598c
HenryTi <liaohengyi@gmail.com>
2024-01-17 23:36:35 -0500

    ok

--- ts/uq/il/Biz/InOut.ts
+++ ts/uq/il/Biz/InOut.ts
@@ -4,19 +4,10 @@ import { IElement } from "../IElement";
 import { BizAct } from "./Base";
 import { Biz } from "./Biz";
 import { BizPhraseType } from "./BizPhraseType";
-import { BizBudValue } from "./Bud";
 import { BizEntity } from "./Entity";
 
-/*
-export interface BizInOutArr {
-    name: string;
-    props: Map<string, BizBudValue>;
-    arrs: { [name: string]: BizInOutArr; };
-}
-*/
 export abstract class BizInOut extends BizEntity {
     protected readonly fields = [];
-    // readonly arrs: { [name: string]: BizInOutArr; } = {};
 }
 
 export class BizIn extends BizInOut {
