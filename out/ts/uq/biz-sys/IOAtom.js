0caa7312451c3186b27a373cfb0bfbe70a413d95
HenryTi <liaohengyi@gmail.com>
2024-01-23 22:14:47 -0500

    ok

--- out/ts/uq/biz-sys/IOAtom.js
+++ out/ts/uq/biz-sys/IOAtom.js
@@ -1,29 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = `
-ATOM $IOOuter {
-    PROP {
-        url CHAR;
-        appKey CHAR;
-    }
-}
-
-DUO $IOOuterApp {
-    I $IOOuter;
-    X $IOApp;
-    PROP {
-        url CHAR;
-        appKey CHAR;
-    }
-}
-
--- obsolete
-ATOM $IOApp {
-}
-
-DUO $IOAppInOut {
-    I $IOApp;
-    X $BizPhrase;
-}
 `;
 //# sourceMappingURL=IOAtom.js.map
