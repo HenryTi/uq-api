4607224c6fea667e384330dd00a5874c6e2189c9
HenryTi <liaohengyi@gmail.com>
2024-10-05 17:36:58 -0400

    PICK TO bud

--- out/ts/uq/parser/Biz/Bin/BinPick.js
+++ out/ts/uq/parser/Biz/Bin/BinPick.js
@@ -6,121 +6,125 @@ const BizPhraseType_1 = require("../../../il/Biz/BizPhraseType");
 const tokens_1 = require("../../tokens");
 const Bud_1 = require("../Bud");
 class PBinPick extends Bud_1.PBizBud {
-    constructor() {
-        super(...arguments);
-        this.from = [];
-    }
     _parse() {
-        this.ts.passKey('from');
-        for (;;) {
-            this.from.push(this.ts.passVar());
-            if (this.ts.token !== tokens_1.Token.BITWISEOR)
-                break;
-            this.ts.readToken();
-        }
-        if (this.ts.token === tokens_1.Token.LBRACE) {
+        if (this.ts.isKeyword('from') === true) {
+            this.from = [];
             this.ts.readToken();
             for (;;) {
-                if (this.ts.token === tokens_1.Token.RBRACE) {
-                    this.ts.readToken();
+                this.from.push(this.ts.passVar());
+                if (this.ts.token !== tokens_1.Token.BITWISEOR)
                     break;
-                }
-                if (this.ts.isKeyword('param') === true) {
-                    this.ts.readToken();
-                    let name = this.ts.passVar();
-                    let ui = this.parseUI();
-                    let pickParam = new il_1.PickParam(this.element.bin, name, ui);
-                    this.context.parseElement(pickParam);
-                    let { params } = this.element;
-                    if (params === undefined) {
-                        params = this.element.params = [];
-                    }
-                    params.push(pickParam);
-                }
-                else if (this.ts.isKeyword('hide') === true) {
-                    this.hides = [];
-                    this.ts.readToken();
-                    if (this.ts.token === tokens_1.Token.LPARENTHESE) {
+                this.ts.readToken();
+            }
+            if (this.ts.token === tokens_1.Token.LBRACE) {
+                this.ts.readToken();
+                for (;;) {
+                    if (this.ts.token === tokens_1.Token.RBRACE) {
                         this.ts.readToken();
-                        for (;;) {
-                            this.hides.push(this.ts.passVar());
-                            if (this.ts.token === tokens_1.Token.COMMA) {
-                                this.ts.readToken();
-                                continue;
+                        break;
+                    }
+                    if (this.ts.isKeyword('param') === true) {
+                        this.ts.readToken();
+                        let name = this.ts.passVar();
+                        let ui = this.parseUI();
+                        let pickParam = new il_1.PickParam(this.element.bin, name, ui);
+                        this.context.parseElement(pickParam);
+                        let { params } = this.element;
+                        if (params === undefined) {
+                            params = this.element.params = [];
+                        }
+                        params.push(pickParam);
+                    }
+                    else if (this.ts.isKeyword('hide') === true) {
+                        this.hides = [];
+                        this.ts.readToken();
+                        if (this.ts.token === tokens_1.Token.LPARENTHESE) {
+                            this.ts.readToken();
+                            for (;;) {
+                                this.hides.push(this.ts.passVar());
+                                if (this.ts.token === tokens_1.Token.COMMA) {
+                                    this.ts.readToken();
+                                    continue;
+                                }
+                                if (this.ts.token === tokens_1.Token.RPARENTHESE) {
+                                    this.ts.readToken();
+                                    break;
+                                }
+                                this.ts.expectToken(tokens_1.Token.COMMA, tokens_1.Token.RPARENTHESE);
                             }
-                            if (this.ts.token === tokens_1.Token.RPARENTHESE) {
-                                this.ts.readToken();
-                                break;
-                            }
-                            this.ts.expectToken(tokens_1.Token.COMMA, tokens_1.Token.RPARENTHESE);
+                        }
+                        else if (this.ts.token === tokens_1.Token.VAR) {
+                            this.hides.push(this.ts.lowerVar);
+                            this.ts.readToken();
                         }
                     }
-                    else if (this.ts.token === tokens_1.Token.VAR) {
-                        this.hides.push(this.ts.lowerVar);
-                        this.ts.readToken();
+                    else {
+                        this.ts.expect('param');
                     }
+                    this.ts.passToken(tokens_1.Token.SEMICOLON);
                 }
-                else {
-                    this.ts.expect('param');
-                }
-                this.ts.passToken(tokens_1.Token.SEMICOLON);
             }
-        }
-        if (this.ts.isKeyword('single') === true) {
-            this.element.single = true;
-            this.ts.readToken();
-        }
-        if (this.ts.prevToken !== tokens_1.Token.RBRACE) {
-            this.ts.passToken(tokens_1.Token.SEMICOLON);
+            if (this.ts.isKeyword('single') === true) {
+                this.element.single = true;
+                this.ts.readToken();
+            }
         }
         else {
-            if (this.ts.isKeyword('to') === true) {
-                this.ts.readToken();
+            if (this.ts.prevLowerVar === 'to') {
                 this.to = this.ts.passVar();
+                this.element.name = this.to;
+                this.ts.mayPassToken(tokens_1.Token.SEMICOLON);
+                return;
             }
-            this.ts.mayPassToken(tokens_1.Token.SEMICOLON);
         }
+        if (this.ts.isKeyword('to') === true) {
+            this.ts.readToken();
+            this.to = this.ts.passVar();
+        }
+        this.ts.mayPassToken(tokens_1.Token.SEMICOLON);
     }
     scan0(space) {
         if (this.element.pick !== undefined)
             return true;
         let ok = true;
         const { biz } = space.uq;
-        const { entityArr, logs, ok: retOk, bizPhraseType, } = biz.sameTypeEntityArr(this.from);
-        if (retOk === false) {
-            this.log(...logs);
-            ok = false;
-        }
-        else {
-            let pickBase;
-            let multipleEntity = false;
-            const bizEntity0 = entityArr[0];
-            switch (bizPhraseType) {
-                default:
-                    this.log(`Can only pick from ATOM, FORK, Options, Pend, or Query`);
-                    ok = false;
-                    break;
-                case BizPhraseType_1.BizPhraseType.atom:
-                    pickBase = new il_1.PickAtom(entityArr);
-                    multipleEntity = true;
-                    break;
-                case BizPhraseType_1.BizPhraseType.fork:
-                    pickBase = new il_1.PickFork(bizEntity0);
-                    break;
-                case BizPhraseType_1.BizPhraseType.pend:
-                    pickBase = new il_1.PickPend(bizEntity0);
-                    break;
-                case BizPhraseType_1.BizPhraseType.query:
-                    pickBase = new il_1.PickQuery(bizEntity0);
-                    break;
-                case BizPhraseType_1.BizPhraseType.options:
-                    pickBase = new il_1.PickOptions(bizEntity0);
-                    break;
-            }
-            this.element.pick = pickBase;
-            if (multipleEntity === false && entityArr.length > 1) {
-                this.log('from only one object');
+        if (this.from !== undefined) {
+            const { entityArr, logs, ok: retOk, bizPhraseType, } = biz.sameTypeEntityArr(this.from);
+            if (retOk === false) {
+                this.log(...logs);
                 ok = false;
+            }
+            else {
+                let pickBase;
+                let multipleEntity = false;
+                const bizEntity0 = entityArr[0];
+                switch (bizPhraseType) {
+                    default:
+                        this.log(`Can only pick from ATOM, FORK, Options, Pend, or Query`);
+                        ok = false;
+                        break;
+                    case BizPhraseType_1.BizPhraseType.atom:
+                        pickBase = new il_1.PickAtom(entityArr);
+                        multipleEntity = true;
+                        break;
+                    case BizPhraseType_1.BizPhraseType.fork:
+                        pickBase = new il_1.PickFork(bizEntity0);
+                        break;
+                    case BizPhraseType_1.BizPhraseType.pend:
+                        pickBase = new il_1.PickPend(bizEntity0);
+                        break;
+                    case BizPhraseType_1.BizPhraseType.query:
+                        pickBase = new il_1.PickQuery(bizEntity0);
+                        break;
+                    case BizPhraseType_1.BizPhraseType.options:
+                        pickBase = new il_1.PickOptions(bizEntity0);
+                        break;
+                }
+                this.element.pick = pickBase;
+                if (multipleEntity === false && entityArr.length > 1) {
+                    this.log('from only one object');
+                    ok = false;
+                }
             }
         }
         if (this.to !== undefined) {
@@ -137,37 +141,41 @@ class PBinPick extends Bud_1.PBizBud {
     }
     scan(space) {
         let ok = true;
-        const { biz } = space.uq;
-        const { logs, ok: retOk } = biz.sameTypeEntityArr(this.from);
-        if (retOk === false) {
-            this.log(...logs);
-            ok = false;
-            return ok;
-        }
-        let { params, bin, pick: pickBase } = this.element;
-        if (params !== undefined) {
-            for (let p of params) {
-                const { name /*, bud, prop*/ } = p;
-                if (pickBase.hasParam(name) === false) {
-                    this.log(`PARAM ${name} is not defined`);
-                    ok = false;
-                }
-                if (p.pelement.scan(space) === false) {
-                    ok = false;
-                }
+        if (this.from !== undefined) {
+            const { biz } = space.uq;
+            const { logs, ok: retOk } = biz.sameTypeEntityArr(this.from);
+            if (retOk === false) {
+                this.log(...logs);
+                ok = false;
+                return ok;
             }
-        }
-        if (this.hides !== undefined) {
-            this.element.hiddenBuds = [];
-            const { hiddenBuds } = this.element;
-            const { pick } = this.element;
-            for (let h of this.hides) {
-                let bud = pick.getBud(h);
-                if (bud === undefined) {
-                    ok = false;
-                    this.log(`${h} not exists`);
+            let { params, pick: pickBase } = this.element;
+            if (pickBase !== undefined) {
+                if (params !== undefined) {
+                    for (let p of params) {
+                        const { name /*, bud, prop*/ } = p;
+                        if (pickBase.hasParam(name) === false) {
+                            this.log(`PARAM ${name} is not defined`);
+                            ok = false;
+                        }
+                        if (p.pelement.scan(space) === false) {
+                            ok = false;
+                        }
+                    }
                 }
-                hiddenBuds.push(bud);
+                if (this.hides !== undefined) {
+                    this.element.hiddenBuds = [];
+                    const { hiddenBuds } = this.element;
+                    const { pick } = this.element;
+                    for (let h of this.hides) {
+                        let bud = pick.getBud(h);
+                        if (bud === undefined) {
+                            ok = false;
+                            this.log(`${h} not exists`);
+                        }
+                        hiddenBuds.push(bud);
+                    }
+                }
             }
         }
         return ok;
--- ts/uq/parser/Biz/Bin/BinPick.ts
+++ ts/uq/parser/Biz/Bin/BinPick.ts
@@ -17,120 +17,129 @@ import { BizEntitySpace } from "../Biz";
 import { PBizBud, PBizBudValue } from "../Bud";
 
 export class PBinPick extends PBizBud<BinPick> {
-    private from: string[] = [];
+    private from: string[];
     private to: string;
     private hides: string[];
     protected _parse(): void {
-        this.ts.passKey('from');
-        for (; ;) {
-            this.from.push(this.ts.passVar());
-            if (this.ts.token !== Token.BITWISEOR) break;
-            this.ts.readToken();
-        }
-        if (this.ts.token === Token.LBRACE as any) {
+        if (this.ts.isKeyword('from') === true) {
+            this.from = [];
             this.ts.readToken();
             for (; ;) {
-                if (this.ts.token === Token.RBRACE as any) {
-                    this.ts.readToken();
-                    break;
-                }
-                if (this.ts.isKeyword('param') === true) {
-                    this.ts.readToken();
-                    let name = this.ts.passVar();
-                    let ui = this.parseUI();
-                    let pickParam = new PickParam(this.element.bin, name, ui);
-                    this.context.parseElement(pickParam);
-                    let { params } = this.element;
-                    if (params === undefined) {
-                        params = this.element.params = [];
-                    }
-                    params.push(pickParam);
-                }
-                else if (this.ts.isKeyword('hide') === true) {
-                    this.hides = [];
-                    this.ts.readToken();
-                    if (this.ts.token === Token.LPARENTHESE) {
+                this.from.push(this.ts.passVar());
+                if (this.ts.token !== Token.BITWISEOR) break;
+                this.ts.readToken();
+            }
+            if (this.ts.token === Token.LBRACE as any) {
+                this.ts.readToken();
+                for (; ;) {
+                    if (this.ts.token === Token.RBRACE as any) {
                         this.ts.readToken();
-                        for (; ;) {
-                            this.hides.push(this.ts.passVar());
-                            if (this.ts.token === Token.COMMA as any) {
-                                this.ts.readToken();
-                                continue;
+                        break;
+                    }
+                    if (this.ts.isKeyword('param') === true) {
+                        this.ts.readToken();
+                        let name = this.ts.passVar();
+                        let ui = this.parseUI();
+                        let pickParam = new PickParam(this.element.bin, name, ui);
+                        this.context.parseElement(pickParam);
+                        let { params } = this.element;
+                        if (params === undefined) {
+                            params = this.element.params = [];
+                        }
+                        params.push(pickParam);
+                    }
+                    else if (this.ts.isKeyword('hide') === true) {
+                        this.hides = [];
+                        this.ts.readToken();
+                        if (this.ts.token === Token.LPARENTHESE) {
+                            this.ts.readToken();
+                            for (; ;) {
+                                this.hides.push(this.ts.passVar());
+                                if (this.ts.token === Token.COMMA as any) {
+                                    this.ts.readToken();
+                                    continue;
+                                }
+                                if (this.ts.token === Token.RPARENTHESE as any) {
+                                    this.ts.readToken();
+                                    break;
+                                }
+                                this.ts.expectToken(Token.COMMA, Token.RPARENTHESE);
                             }
-                            if (this.ts.token === Token.RPARENTHESE as any) {
-                                this.ts.readToken();
-                                break;
-                            }
-                            this.ts.expectToken(Token.COMMA, Token.RPARENTHESE);
+                        }
+                        else if (this.ts.token === Token.VAR) {
+                            this.hides.push(this.ts.lowerVar);
+                            this.ts.readToken();
                         }
                     }
-                    else if (this.ts.token === Token.VAR) {
-                        this.hides.push(this.ts.lowerVar);
-                        this.ts.readToken();
+                    else {
+                        this.ts.expect('param');
                     }
+                    this.ts.passToken(Token.SEMICOLON);
                 }
-                else {
-                    this.ts.expect('param');
-                }
-                this.ts.passToken(Token.SEMICOLON);
             }
-        }
-        if (this.ts.isKeyword('single') === true) {
-            this.element.single = true;
-            this.ts.readToken();
-        }
-        if (this.ts.prevToken !== Token.RBRACE) {
-            this.ts.passToken(Token.SEMICOLON);
+            if (this.ts.isKeyword('single') === true) {
+                this.element.single = true;
+                this.ts.readToken();
+            }
         }
         else {
-            if (this.ts.isKeyword('to') === true) {
-                this.ts.readToken();
+            if (this.ts.prevLowerVar === 'to') {
                 this.to = this.ts.passVar();
+                this.element.name = this.to;
+                this.ts.mayPassToken(Token.SEMICOLON);
+                return;
             }
-            this.ts.mayPassToken(Token.SEMICOLON);
         }
+
+        if (this.ts.isKeyword('to') === true) {
+            this.ts.readToken();
+            this.to = this.ts.passVar();
+        }
+        this.ts.mayPassToken(Token.SEMICOLON);
     }
 
     scan0(space: Space): boolean {
         if (this.element.pick !== undefined) return true;
         let ok = true;
         const { biz } = space.uq;
-        const { entityArr, logs, ok: retOk, bizPhraseType, }
-            = biz.sameTypeEntityArr(this.from);
-        if (retOk === false) {
-            this.log(...logs);
-            ok = false;
-        }
-        else {
-            let pickBase: PickBase;
-            let multipleEntity = false;
-            const bizEntity0 = entityArr[0];
-            switch (bizPhraseType) {
-                default:
-                    this.log(`Can only pick from ATOM, FORK, Options, Pend, or Query`);
-                    ok = false;
-                    break;
-                case BizPhraseType.atom:
-                    pickBase = new PickAtom(entityArr as BizAtom[]);
-                    multipleEntity = true;
-                    break;
-                case BizPhraseType.fork:
-                    pickBase = new PickFork(bizEntity0 as BizFork);
-                    break;
-                case BizPhraseType.pend:
-                    pickBase = new PickPend(bizEntity0 as BizPend);
-                    break;
-                case BizPhraseType.query:
-                    pickBase = new PickQuery(bizEntity0 as BizQueryTable);
-                    break;
-                case BizPhraseType.options:
-                    pickBase = new PickOptions(bizEntity0 as BizOptions);
-                    break;
-            }
-            this.element.pick = pickBase;
-            if (multipleEntity === false && entityArr.length > 1) {
-                this.log('from only one object');
+        if (this.from !== undefined) {
+            const { entityArr, logs, ok: retOk, bizPhraseType, }
+                = biz.sameTypeEntityArr(this.from);
+            if (retOk === false) {
+                this.log(...logs);
                 ok = false;
+            }
+            else {
+                let pickBase: PickBase;
+                let multipleEntity = false;
+                const bizEntity0 = entityArr[0];
+                switch (bizPhraseType) {
+                    default:
+                        this.log(`Can only pick from ATOM, FORK, Options, Pend, or Query`);
+                        ok = false;
+                        break;
+                    case BizPhraseType.atom:
+                        pickBase = new PickAtom(entityArr as BizAtom[]);
+                        multipleEntity = true;
+                        break;
+                    case BizPhraseType.fork:
+                        pickBase = new PickFork(bizEntity0 as BizFork);
+                        break;
+                    case BizPhraseType.pend:
+                        pickBase = new PickPend(bizEntity0 as BizPend);
+                        break;
+                    case BizPhraseType.query:
+                        pickBase = new PickQuery(bizEntity0 as BizQueryTable);
+                        break;
+                    case BizPhraseType.options:
+                        pickBase = new PickOptions(bizEntity0 as BizOptions);
+                        break;
+                }
+                this.element.pick = pickBase;
+                if (multipleEntity === false && entityArr.length > 1) {
+                    this.log('from only one object');
+                    ok = false;
+                }
             }
         }
         if (this.to !== undefined) {
@@ -148,37 +157,41 @@ export class PBinPick extends PBizBud<BinPick> {
 
     scan(space: Space): boolean {
         let ok = true;
-        const { biz } = space.uq;
-        const { logs, ok: retOk } = biz.sameTypeEntityArr(this.from);
-        if (retOk === false) {
-            this.log(...logs);
-            ok = false;
-            return ok;
-        }
-        let { params, bin, pick: pickBase } = this.element;
-        if (params !== undefined) {
-            for (let p of params) {
-                const { name/*, bud, prop*/ } = p;
-                if (pickBase.hasParam(name) === false) {
-                    this.log(`PARAM ${name} is not defined`);
-                    ok = false;
-                }
-                if (p.pelement.scan(space) === false) {
-                    ok = false;
-                }
+        if (this.from !== undefined) {
+            const { biz } = space.uq;
+            const { logs, ok: retOk } = biz.sameTypeEntityArr(this.from);
+            if (retOk === false) {
+                this.log(...logs);
+                ok = false;
+                return ok;
             }
-        }
-        if (this.hides !== undefined) {
-            this.element.hiddenBuds = [];
-            const { hiddenBuds } = this.element;
-            const { pick } = this.element;
-            for (let h of this.hides) {
-                let bud = pick.getBud(h);
-                if (bud === undefined) {
-                    ok = false;
-                    this.log(`${h} not exists`);
+            let { params, pick: pickBase } = this.element;
+            if (pickBase !== undefined) {
+                if (params !== undefined) {
+                    for (let p of params) {
+                        const { name/*, bud, prop*/ } = p;
+                        if (pickBase.hasParam(name) === false) {
+                            this.log(`PARAM ${name} is not defined`);
+                            ok = false;
+                        }
+                        if (p.pelement.scan(space) === false) {
+                            ok = false;
+                        }
+                    }
                 }
-                hiddenBuds.push(bud);
+                if (this.hides !== undefined) {
+                    this.element.hiddenBuds = [];
+                    const { hiddenBuds } = this.element;
+                    const { pick } = this.element;
+                    for (let h of this.hides) {
+                        let bud = pick.getBud(h);
+                        if (bud === undefined) {
+                            ok = false;
+                            this.log(`${h} not exists`);
+                        }
+                        hiddenBuds.push(bud);
+                    }
+                }
             }
         }
         return ok;
